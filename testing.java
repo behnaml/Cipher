23b0ed2bdb284d7030a59ac88b171082bbe7664f
bellaotter <39567669+bellaotter@users.noreply.github.com>
2018-06-05 08:38:40 -0700

    Update testing.java

--- testing.java
+++ testing.java
@@ -1,7 +1,7 @@
-
+//this was our quick testing class to find problems in each cipher type
 public class testing {
 public static void main(String[] args) {
 	leila a = new leila("", "house-winter- hawaii-house-");
-	System.out.println(a);
+	System.out.println(a); //prints toString of contructed cipher
 }
 }
